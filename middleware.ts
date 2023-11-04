cef353b03153e3d15b353ba57931fb8b03f47c12
Stepan Krousky <stepan.krousky@seznam.cz>
2023-11-04 18:09:34 +0100

    ...

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,25 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-export function middleware(request: NextRequest) {
+async function middleware1(request: NextRequest) {
   const url = request.url;
-  console.log('middleware => ' + url);
+  console.log('middleware1 => ', { url });
 
   return NextResponse.next();
 }
 
+async function middleware2(request: NextRequest) {
+  const pathname = request.nextUrl.pathname;
+  console.log('middleware2 => ', { pathname });
+
+  return NextResponse.next();
+}
+
+export async function middleware(request: NextRequest) {
+  await middleware1(request);
+  await middleware2(request);
+}
+
 export const config = {
-  matcher: ['/about'],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)',],
 };
