dc577534e14015500fe6c31e41293f831cab7a46
Stepan Krousky <stepan.krousky@seznam.cz>
2023-11-04 23:38:20 +0100

    ...

--- middlewares/middleware1.ts
+++ middlewares/middleware1.ts
@@ -1,10 +1,13 @@
-import { NextFetchEvent, NextMiddleware, NextRequest } from "next/server";
+import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
+import { CustomMiddleware } from "./chain";
 
-export  function withMiddleware1(middleware: NextMiddleware) {
+export  function withMiddleware1(middleware: CustomMiddleware) {
     return async (request: NextRequest, event: NextFetchEvent) => {
+      const response = NextResponse.next()
+
       const url = request.url;
       console.log('middleware1 => ', { url });
   
-      return middleware(request, event);
+      return middleware(request, event, response);
     };
   }
