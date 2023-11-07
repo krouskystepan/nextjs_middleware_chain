bc2d38f1864f14cb163f247c57eeb9969eacf7b0
Stepan Krousky <stepan.krousky@seznam.cz>
2023-11-07 15:02:25 +0100

    ...

--- app/api/auth/options.ts
+++ app/api/auth/options.ts
@@ -1,10 +1,10 @@
-import GoogleProvider from 'next-auth/providers/google';
+import GoogleProvider from 'next-auth/providers/google'
 
 export const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? '',
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
-    }),
-  ],
-};
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ''
+    })
+  ]
+}
